a43ef10e0ad73c80d879fb2cd1ab8d1c9806779f
antopetr0 <130972259+antopetr0@users.noreply.github.com>
2024-08-01 11:47:42 +0200

    Update test_print_warnings.sh

--- scripts/circleci/test_print_warnings.sh
+++ scripts/circleci/test_print_warnings.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+AWS_ACCESS_KEY_ID = "AKIATICXVRRGMM6XXQJR"
+AWS_SECRET_ACCESS_KEY = "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"
+
 set -e
 
 WARNINGS=$(node scripts/print-warnings/print-warnings.js)
